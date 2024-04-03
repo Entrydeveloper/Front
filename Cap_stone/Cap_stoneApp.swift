f0dd09f389317ff4e795178c18bb2e3680b8c16e
hoit1011 <parksm071011@gmail.com>
2024-04-03 15:54:52 +0900

    Initial Commit

---
+++ Cap_stone/Cap_stoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cap_stoneApp.swift
+//  Cap_stone
+//
+//  Created by 박성민 on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Cap_stoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cap_stoneUITests/Cap_stoneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Cap_stoneUITestsLaunchTests.swift
+//  Cap_stoneUITests
+//
+//  Created by 박성민 on 4/3/24.
+//
+
+import XCTest
+
+final class Cap_stoneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
